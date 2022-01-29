aaa94ad64bacdbe3ce77678ac8fb6e06efc45cce
Sinan Kapusız <sinankapusiz@outlook.com>
2022-01-29 14:43:11 +0300

    Console 1.Örnek
    PatikaDev/C#/Console/ Örnekleri.

---
+++ C# Ders Ödevleri/Console Programlama/Örnek 1/Ornek1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ornek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen isminizi giriniz : "); //.WriteLine yerine .Write yazılma nedeni: mesaj ile girişin tek satırda olmasını istemem.
+            string isim = Console.ReadLine();
+            Console.Write("Lütfen Soyisminizi giriniz : ");
+            string soyIsim = Console.ReadLine();
+            Console.WriteLine("PatikaDev Hoşgeldin {0} {1}",isim,soyIsim);
+        }
+    }
+}
