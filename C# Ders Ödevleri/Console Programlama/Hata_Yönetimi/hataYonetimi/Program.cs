3ed990cac1f63ef130968d4d877b0158e383de13
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-05 23:42:31 +0300

    If Else
    If else ve Ternary örneği.

--- C# Ders Ödevleri/Console Programlama/Hata_Yönetimi/hataYonetimi/Program.cs
+++ C# Ders Ödevleri/Console Programlama/Hata_Yönetimi/hataYonetimi/Program.cs
@@ -26,22 +26,24 @@ namespace hataYonetimi
                 //int a=int.Parse(null);
                 //int a = int.Parse("test");
                 int a = int.Parse("-2000000000000000");
+                int b = 25;
+                Console.WriteLine(b);
             }
             catch (ArgumentNullException ex)
             {
                 Console.WriteLine("Boş bir değer girdiniz.");
                 Console.WriteLine(ex);
             }
-            catch (FormatException ex)
-            {
-                Console.WriteLine("Veri tipi uygun değil.");
-                Console.WriteLine(ex);
-            }
-            catch (OverflowException ex)
-            {
-                Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz.");
-                Console.WriteLine(ex);
-            }
+            // catch (FormatException ex)
+            // {
+            //     Console.WriteLine("Veri tipi uygun değil.");
+            //     Console.WriteLine(ex);
+            // }
+            // catch (OverflowException ex)
+            // {
+            //     Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz.");
+            //     Console.WriteLine(ex);
+            // }
 
         }
     }
---
+++ C# Ders Ödevleri/Console Programlama/If_Else(Koşullar)/ıf_else/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ıf_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time <= 11)
+                Console.WriteLine("Günaydın!");
+            else if (time <= 18)
+                Console.WriteLine("İyi Günler!");
+            else
+                Console.WriteLine("iyi Geceler!");
+
+            string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";
+
+            sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "İyi Günler" : "İyi Geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
