640a59c895519d0a3eac88d97fcf499903528011
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-06 14:28:17 +0300

    Döngüler 2
    While, Foreach.

---
+++ C# Ders Ödevleri/Console Programlama/Döngüler 2/Donguler2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Donguler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam=0;
+            while (sayac<= sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //'a' dan 'z' ye kadar tüm harfleri console yazdır.
+
+            char character='a';
+            while (character<'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+        }
+    }
+}
